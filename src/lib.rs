c4e16cae5a51c50d1d5d9a5b220f222e5c5d704c
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-05-26 13:44:23 +0900

    ドキュメントにファイルを切り替えるタイミングの仕様を記述

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,20 @@
 //! 
 //! このクレートには、`LRFAppender(Limited Rolling File Appender)`構造体が
 //! 含まれており、この構造体は、ログを記録することを目的としている。
+//! `LRFAppender`は、`tracing`クレートの`tracing_appender::rolling::RollingFileAppender
+//! を内部にもち（合成）、多くの処理を`RollingFileAppender`に移譲している。
 //! 
 //! `LRFAppender`は、以下いずれかの条件で、ログの記録を新しいファイルに切り替える。 
 //! 
 //! - 日付が変わったとき
 //! - ファイルに指定されたサイズのログを出力したとき
-
+//!
+//! また、`LRFAppender`は、残しておく最大ファイル数を持つ。
+//! `LRFAppender`は、新しいファイルに切り替えるとき、現在のファイルを含めて、ファイル数が
+//! 最大ファイル数を超えた場合、最も古いファイルから削除する。
 
 /// ファイルを切り替えるタイミング
-pub enum RollingTiming {
+pub enum SwitchTiming {
     /// 日付が変わったときにファイルを切り替え
     Daily,
     /// 指定されたサイズ書き込んだときにファイルを切り替え
