d487b00a415597874b33c87a6ac0b529f7fba12a
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-05-28 16:16:35 +0900

    lib.rsのドキュメントを削除

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,2 @@
-//! Limited Rolling File Appender
-//!
-//! このクレートには、ログを記録することを目的とする構造体が含まれている。
-//! ログを記録することを目的とした構造体は、`tracing-appender`クレートの`RollingFileAppender`
-//! を内部にもち（合成）、! 多くの処理を`RollingFileAppender`に移譲している。
 pub mod appenders;
 mod sync;
